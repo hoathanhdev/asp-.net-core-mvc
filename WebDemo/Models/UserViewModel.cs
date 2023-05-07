4e5adb82ec686edd19a49948fbdabbafca0563c3
hoathanhdev <hoaphamcd1206@gmail.com>
2023-05-07 12:47:16 +0700

    change Usermodelview

--- WebDemo/Models/UserViewModel.cs
+++ WebDemo/Models/UserViewModel.cs
@@ -15,22 +15,22 @@ namespace WebDemo.Models
 
         [DisplayName("Họ và Tên")]
         [MaxLength(125,ErrorMessage ="Họ và tên không được quá 125 kí tự")]
-        public int uFullName { get; set; }
+        public string uFullName { get; set; }
 
         [DisplayName("Giới tính")]
-        public int uSex { get; set; }
+        public bool uSex { get; set; }
 
         [DisplayName("Thông tin tài khoản")]
         [MaxLength(1000, ErrorMessage = "Thông tin tài khoản không quá 1000 kí tự")]
-        public int uInformation { get; set; }
+        public string uInformation { get; set; }
 
         [DisplayName("Điện thoại")]
         [MaxLength(32, ErrorMessage = "Điện thoại không được quá 32 kí tự")]
-        public int uMobile { get; set; }
+        public string uMobile { get; set; }
 
         [DisplayName("Email")]
         [MaxLength(125, ErrorMessage = "Email không được quá 125 kí tự")]
-        public int uEmail { get; set; }
+        public string uEmail { get; set; }
 
         public DateTime CreatedDate { get; set; }
 
